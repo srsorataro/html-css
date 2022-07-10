82a116ccd699947123ce72f47dbbe3ab5e311cae
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-10 14:53:23 -0300

    buto

--- ex39/menu.js
+++ ex39/menu.js
@@ -1,7 +1,12 @@
 const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu(){
+    if(Event.type ==='touchstars')Event.preventDefault
     const nav= document.getElementById('nav');
     nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    Event.currentTarget.setAttribute('aria-expanded',active)
 
 }
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstars', toggleMenu);
+
