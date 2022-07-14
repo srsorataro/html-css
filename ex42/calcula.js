a4327ba8ccd06231f30c8f64355c92d9b92a291d
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-14 07:19:37 -0300

    454

--- ex42/calcula.js
+++ ex42/calcula.js
@@ -1,7 +1,32 @@
-class Calculator(){
+class Calculator{
     constructor(previousOperandTextElement,currentOperandTextElement){
-        this.previousOperandTextElement =previousOperandTextElement
+        this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation =  undefined
+
+    }
+    delete(){
+        this.currentOperand =this this.currentOperand.toString().slice(0,1)
+
+    }
+    appendNumber(number){
+        this.currentOperand = number
+
+    }
+    chooseOperation(operation){
+
+    }
+    compute(){
+
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.innerHTML =this.currentOperand
+
     }
 }
 const numberButtons = document.querySelectorAll('[data-number]');
@@ -11,4 +36,14 @@ const deleteButton = document.querySelector('[data-delete');
 const allClearButton = document.querySelector('[data-all-Clear');
 const previousOperandTextElement = document.querySelector('[data-previous-Operand');
 const currentOperandTextElement = document.querySelector('[data-current-operand');
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button =>{
+    button.addEventListener(click,() => {
+        calculator.appendNumber(button.innerHTML)
+        calculator.updateDisplay()
+    })
+
+
+})
 
