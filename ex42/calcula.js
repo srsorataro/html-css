ff16f17c8c327404b4940f7e8ca9d54939c5c8f5
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-15 07:18:18 -0300

    Update calcula.js

--- ex42/calcula.js
+++ ex42/calcula.js
@@ -11,29 +11,53 @@ class Calculator{
         this.operation = undefined
       }
     delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
 
     }
     appendNumber(number){
-        if(number === '.' && this.currentOperand.includes('.'))return
+        if(number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
     chooseOperation(operation){
-        if(this.currentOperand === '')return
+        if(this.currentOperand === '') return
         if(this.previousOperand !== ''){
             this.compute()
         }
 
         this.operation = operation
-        this.previousOperand =this.currentOperand
+        this.previousOperand = this.currentOperand
         this.currentOperand =''
 
     }
-    compute(){
-
-    }
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+          case '+':
+            computation = prev + current
+            break
+          case '-':
+            computation = prev - current
+            break
+          case '*':
+            computation = prev * current
+            break
+          case '÷':
+            computation = prev / current
+            break
+          default:
+            return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+      }
     updateDisplay(){
         
         this.currentOperandTextElement.innerText =this.currentOperand
+        if(this.operation != null)
         this.previousOperandTextElement.innerText =this.previousOperand
 
     }
@@ -60,4 +84,11 @@ operationButtons.forEach(button => {
         calculator.updateDisplay()
     })
 })
-equalsButton.addEventListener('click',button)
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
