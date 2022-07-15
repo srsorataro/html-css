a77ad61b3a81baf10a90be5b9cb495d51e7ad540
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-15 13:12:38 -0300

    6

--- ex42/calcula.js
+++ ex42/calcula.js
@@ -54,12 +54,33 @@ class Calculator{
         this.operation = undefined
         this.previousOperand = ''
       }
+      getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits =  parseFloat(stringNumber.split('.')[0])
+        const decimalDigits =  stringNumber.split('.')[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        }else{
+            integerDisplay = integerDigits.toLocaleString('en',{ maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+
+        }else{
+            return integerDisplay
+        }
+      
+      }
     updateDisplay(){
         
-        this.currentOperandTextElement.innerText =this.currentOperand
-        if(this.operation != null)
-        this.previousOperandTextElement.innerText =this.previousOperand
-
+        this.currentOperandTextElement.innerText = 
+        this. getDisplayNumber (this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = 
+            ` ${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        }
+        
     }
 
 }
@@ -90,5 +111,9 @@ equalsButton.addEventListener('click', button => {
 })
 deleteButton.addEventListener('click', button => {
     calculator.delete()
+    calculator.updateDisplay()
+})
+allclearButton.addEventListener('click', button => {
+    calculator.clear()
     calculator.updateDisplay()
 })
