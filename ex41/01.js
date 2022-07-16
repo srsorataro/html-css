2da03f4bbf65c6acdc3a414020606dbf822a05a5
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-16 10:02:47 -0300

    Update 01.js

--- ex41/01.js
+++ ex41/01.js
@@ -1,8 +1,15 @@
-const array = [ 70,70,80]
-console.log(mediaDoAluno(array))
-function mediaDoAluno(notas){
-    for( array / notas === length(array)  < 60 )
-        return'sua nota e D'
-    
+somar(10)
+function somar(limite){
+    let mutiploDe3 = 0
+    let mutiploDe5 = 0
+    for(i = 0; i <= limite; i++){
+
+        if( i % 3 === 0)
+        mutiploDe3 += i
+        if(i % 5 === 0)
+        mutiploDe5 += i
+
+    } 
+    console.log(mutiploDe3 +mutiploDe5)   
 
 }
