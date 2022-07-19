6c16de1ba961cbd36f6f5d16f6724843370eb3ab
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-19 07:43:26 -0300

    Update 01.js

--- ex41/01.js
+++ ex41/01.js
@@ -1,17 +1,21 @@
-const celular ={
-    marcaCelular: 'ASUS',
-    tamanhoTela:{
-        vertical: 155,
-        horizontal: 75
-    },
-    ligar: function(){
-        console.log('Fazendo ligação..')
-    }
+function Endereço(rua,cidade,cep){
+    this.rua = rua,
+    this.cidade = cidade,
+    this.cep = cep
 }
- const novoObjeto = Object.assign({
-    bateria:5000
- }, celular)
- console.log(novoObjeto)
+const endereço1 = new Endereço('a','b','c')
+const endereço2 = new Endereço('a','b','c')
 
-const obejeto2 = {...celular}
-console.log(obejeto2)
+function sãoIguais(endereço1,endereço2){
+        return  endereço1.rua === endereço2.rua &&
+                endereço1.cidade === endereço2.cidade &&
+                endereço1.cep === endereço2.cep
+    
+}
+console.log(sãoIguais(endereço1,endereço2))
+
+function temEndereçoMemoriaIguais(endereço1,endereço2){
+    return endereço1 === endereço2
+
+}
+console.log(temEndereçoMemoriaIguais(endereço1,endereço2))
