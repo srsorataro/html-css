08313281b62268f9504da90ab2c68f9551132cf4
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-19 15:32:48 -0300

    Update 01.js

---
+++ ex41/01.js
@@ -0,0 +1,8 @@
+const numeros = [1,2,3,4,5,6,47,98]
+
+const combinado  = numeros.join('💗')
+console.log(combinado);
+const frase  = "Olá bem vindo ao curso"
+const resultado = frase.split(' ')
+console.log(resultado)
+console.log(resultado.join('💢'))
