908c338a39d8e395a664544ccc1f6538f0e6d880
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-07-19 09:46:21 -0300

    Update 01.js

--- ex41/01.js
+++ ex41/01.js
@@ -1,21 +1,5 @@
-function Endereço(rua,cidade,cep){
-    this.rua = rua,
-    this.cidade = cidade,
-    this.cep = cep
-}
-const endereço1 = new Endereço('a','b','c')
-const endereço2 = new Endereço('a','b','c')
-
-function sãoIguais(endereço1,endereço2){
-        return  endereço1.rua === endereço2.rua &&
-                endereço1.cidade === endereço2.cidade &&
-                endereço1.cep === endereço2.cep
-    
-}
-console.log(sãoIguais(endereço1,endereço2))
-
-function temEndereçoMemoriaIguais(endereço1,endereço2){
-    return endereço1 === endereço2
-
-}
-console.log(temEndereçoMemoriaIguais(endereço1,endereço2))
+let faixas =[
+    {
+        tooltips: 'Até 700 Reais', minimo: 0, maximo: 7000
+    }
+]
