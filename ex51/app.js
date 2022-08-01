eca378d28368bd44fb59459b1253731e61272875
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-01 13:17:25 -0300

    63

--- ex51/app.js
+++ ex51/app.js
@@ -1,11 +1,17 @@
-const colors = ["green","red", "rgba(133,122,200)","#f15025"]
+const colors = ["green","red", "rgba(133,122,200)","#f15025","yellow"]
 const btn =document.getElementById('btn')
 const color =document.querySelector("color")
 
 btn.addEventListener('click', function(){
     //pegue um número aleatório entre 0 - 3 
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
 
-   const randomNumber = 2
    document.body.style.backgroundColor=colors[randomNumber]
    color.textContent = colors[randomNumber]
+
 })
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+    
+}
