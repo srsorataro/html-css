e6a0160d434bdf1801d162807fb292b50ae03100
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-03 05:52:31 -0300

    63

---
+++ ex53/app.js
@@ -0,0 +1,9 @@
+//set initial count
+let count = 0 
+//select value and butons
+const value = document.querySelector('#value')
+const btns  = document.querySelectorAll('.btn')
+
+btns.forEach(function (){
+    
+})
