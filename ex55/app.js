794b1b31305ccb6da5905af98a4df4fa87b076f1
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-07 16:37:47 -0300

    775

--- ex55/app.js
+++ ex55/app.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.links')
+
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+   
+    
+    
 })
