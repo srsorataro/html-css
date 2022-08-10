9c2be75e6c41c85a036a918bcba57cefb091e9e7
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-10 07:39:14 -0300

    45

--- ex55/app.js
+++ ex55/app.js
@@ -1,11 +1,26 @@
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-  })
-  const hamburgue =document.querySelector('.burgue')
-  const navmenu = document.querySelector('nav')
+window.addEventListener("scroll", () =>{
 
-  hamburgue.addEventListener("click", () =>{
-    hamburgue.classList.toggle("active")
-    navmenu.classList.toggle("active")
-  })
+  var header = document.querySelector('header')
+  header.classList.toggle("sticky",window.scrollY  > 0)
+})
+const btnMobile = document.getElementById('btn-mobile')
+function toggleMenu(event){
+  if(event === 'touchstart')event.preventDefault()
+  const nav = document.getElementById('nav')
+  nav.classList.toggle("active")
+  const active =nav.classList.contains('active')
+  const bot = document.getElementById('hamburgue')
+  bot.classList.toggle("active")
+  event.currentTarget.setAtribute('aria-expanded', active)
+  if(active){
+    event.currentTarget.setAtribute('aria-label','fechar menu')
+
+  }else{
+    event.currentTarget.setAtribute('aria-label','abrir menu')
+    
+  }
+
+ 
+}
+btnMobile.addEventListener('click',toggleMenu)
+btnMobile.addEventListener('touchstart',toggleMenu)
