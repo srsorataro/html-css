02d3c22c1e9ec2c81bd354fe2109e7d42d49086c
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-10 19:01:47 -0300

    545

--- ex55/app.js
+++ ex55/app.js
@@ -1,1 +1,6 @@
-
+const btnMobile = document.querySelector('btn-menu')
+function toggleMenu(){
+    const nav = document.querySelector('nav1')
+    nav.classList.toggle('active')
+}
+btnMobile.addEventListener('click',toggleMenu)
