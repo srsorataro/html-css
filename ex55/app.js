1ec6c1be29655f56a2e68a192756dfdb7a13f1e3
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-13 13:19:22 -0300

    4465

--- ex55/app.js
+++
@@ -1,7 +0,0 @@
-const hamb  =document.querySelector('.hamburgue')
-const Menu1  =document.querySelector('.menu')
-hamb.addEventListener('click', () =>{
-    hamb.classList.toggle('active')
-    Menu1.classList.toggle('active')
-})
-
