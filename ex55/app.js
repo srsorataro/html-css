d3751c152dd82d57a8f8ac598693000d5d542966
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-19 08:32:17 -0300

    46

--- ex55/app.js
+++ ex55/app.js
@@ -1,6 +1,9 @@
 const btnToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 const navC = document.querySelector('.nav-center');
+const soI = document.querySelector('.social-icons');
+
+
 
 
 
@@ -8,5 +11,7 @@ btnToggle.addEventListener('click', () =>{
     btnToggle.classList.toggle('active')
     links.classList.toggle('active')
     navC.classList.toggle('active')
+    soI.classList.toggle('active')
+    
     
 })
