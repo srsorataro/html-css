de6aa163fcdd98957cd1188b8bdc715c5f797f55
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-23 15:39:45 -0300

    Update app.js

--- ex61/app.js
+++ ex61/app.js
@@ -1,12 +1,15 @@
 const questions = document.querySelectorAll('.question')
 
+
 questions.forEach(function(question){
     const btn = question.querySelector('.question-btn')
-    btn.addEventListener("click", ()=>{
+    btn.addEventListener('click',()=>{
         questions.forEach(function(item){
             if(item !== question){
                 item.classList.remove('active')
+
             }
+            
         })
         question.classList.toggle('active')
     })
