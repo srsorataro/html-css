db1e9c72a210708f2f0d8a8e712c3983be777396
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-08-23 08:30:25 -0300

    4546

--- ex60/model.js
+++ ex60/model.js
@@ -2,12 +2,15 @@ const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-container");
 const closeBtn = document.querySelector(".close-btn");
 const bann = document.querySelector(".banner");
+const overL = document.querySelector(".modal-overlay");
 
 modalBtn.addEventListener("click", function () {
 modal.classList.add("active");
 bann.classList.add("active");
+overL.classList.add("active");
 });
 closeBtn.addEventListener("click", function () {
   modal.classList.remove("active");
   bann.classList.remove("active");
+  overL.classList.remove("active");
 });
