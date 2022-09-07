5ded8ee21c4a154f713d46750115c2829c1aa12a
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-09-07 12:36:58 -0300

    545

--- ex67/js/app.js
+++ ex67/js/app.js
@@ -4,27 +4,3 @@
 //offfsettop - A Number, representing the  top position  of the element, in pixels
 
 
-
-const  menu  = document.querySelector(".nav-toggle")
-const  links = document.querySelector(".links-container")
-const  cont = document.querySelector(".container")
-const  nav = document.querySelector(".nav-center")
-
-
-
-menu.addEventListener('click',()=>{
-    //menu.classList.toggle('active')
-    //links.classList.toggle('active')
-    //cont.classList.toggle('active')
-   // nav.classList.toggle('active')
-   const containerHeight = nav.getBoundingClientRect()
-   console.log(containerHeight)
-  
-})
-
-//****************set date************
-const date = document.getElementById("date")
-date.innerHTML = new Date().getFullYear()
-//***********fixed navbar */
-//****************smooth scroll */
-// select links*/
