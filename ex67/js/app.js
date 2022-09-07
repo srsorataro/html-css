f49adc7c69ccdf8b19cf4e0704b5b464d05bd915
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-09-07 08:07:16 -0300

    456423

--- ex67/js/app.js
+++ ex67/js/app.js
@@ -1,3 +1,10 @@
+//Element.getBoundClientReact() method return the side oof an  element and ists position relativve to the viewport.
+//pageYOffset is a read - only window property that return the number of pixels the document has been scrolled vertically.
+//slice  extracts a section of a string without modifying original string
+//offfsettop - A Number, representing the  top position  of the element, in pixels
+
+
+
 const  menu  = document.querySelector(".nav-toggle")
 const  links = document.querySelector(".links-container")
 const  cont = document.querySelector(".container")
@@ -6,9 +13,18 @@ const  nav = document.querySelector(".nav-center")
 
 
 menu.addEventListener('click',()=>{
-    menu.classList.toggle('active')
-    links.classList.toggle('active')
-    cont.classList.toggle('active')
-    nav.classList.toggle('active')
+    //menu.classList.toggle('active')
+    //links.classList.toggle('active')
+    //cont.classList.toggle('active')
+   // nav.classList.toggle('active')
+   const containerHeight = nav.getBoundingClientRect()
+   console.log(containerHeight)
   
 })
+
+//****************set date************
+const date = document.getElementById("date")
+date.innerHTML = new Date().getFullYear()
+//***********fixed navbar */
+//****************smooth scroll */
+// select links*/
