4779bd2160242e8b194bdc25f3c5e427ba4817a1
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-09-09 13:41:00 -0300

    4654

--- ex67/js/app.js
+++ ex67/js/app.js
@@ -8,13 +8,42 @@ const btn = document.querySelector('.nav-toggle');
 const linksContainer= document.querySelector('.links-container');
 const center= document.querySelector('.nav-center');
 const menu= document.querySelector('.menu');
+const links= document.querySelector('.links');
 
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 
 btn.addEventListener('click', ()=>{
-    linksContainer.classList.toggle('show-links')
-    center.classList.toggle('active')
-    menu.classList.toggle('active')
-
+   //linksContainer.classList.toggle('show-links')
+   //center.classList.toggle('active')
+    //menu.classList.toggle('active')
+    const containerHeight = links.getBoundingClientRect().height
+    const linksHeight = linksContainer.getBoundingClientRect().height;
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`
+    }
+    else{
+        linksContainer.style.height = 0
+    }
   
+
+})
+const navbar = document.getElementById('nav')
+const topLinks = document.querySelector('.top-link')
+window.addEventListener('scroll',()=>{
+    const  scrollHeight  = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight >  navHeight){
+        navbar.classList.add('fixed-nav')
+
+    }
+    else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeight > 500){
+        topLinks.classList.remove('show-links')
+    }
+    
 })
