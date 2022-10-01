e81d5830cb9b2d2bcb3f4bd06f3b29d87e051464
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-01 17:33:38 -0300

    654

--- exAlura2/js/app.js
+++ exAlura2/js/app.js
@@ -1,10 +1,12 @@
 const botao = document.querySelector(".botao");
 const nav = document.querySelector('.nav');
 const cab = document.querySelector('.cabeca');
+const pro = document.querySelector('.produtos');
 
 botao.addEventListener('click', ()=>{
     botao.classList.toggle('active');
     nav.classList.toggle('active');
     cab.classList.toggle('active');
+    pro.classList.toggle('active');
     
 })
