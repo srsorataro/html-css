0bc26fbc8eb84a855a861014d66421b08efde68a
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-10 07:42:29 -0300

    154521

--- exAlura05/js/main.js
+++ exAlura05/js/main.js
@@ -1,46 +1,53 @@
 
+const teclas =[];
+const sons =[];
+
+ //butoes
+ 
+ document.querySelector('.tecla_pom').onclick = butao;
+ document.querySelector('.tecla_clap').onclick = butao1;
+ document.querySelector('.tecla_tim').onclick = butao2;
+ document.querySelector('.tecla_puff').onclick = butao3;
+ document.querySelector('.tecla_toim').onclick = butao4;
+ document.querySelector('.tecla_splash').onclick = butao5;
+ document.querySelector('.tecla_psh').onclick = butao6;
+ document.querySelector('.tecla_tic').onclick = butao7;
+ document.querySelector('.tecla_tom').onclick = butao8;
+
+//Sons
 function butao(){
-    document.querySelector('.tecla_pom');
     document.querySelector('#som_tecla_pom').play();
-
-
 }
+
 function butao1(){
-    document.querySelector('.tecla_clap');
     document.querySelector('#som_tecla_clap').play();
 }
+
 function butao2(){
-    document.querySelector('.tecla_tim');
     document.querySelector('#som_tecla_tim').play();
 }
 
 function butao3(){
-    document.querySelector('.tecla_puff');
     document.querySelector('#som_tecla_puff').play();
 }
 
 function butao4(){
-    document.querySelector('.tecla_splash');
     document.querySelector('#som_tecla_splash').play();
 }
 
 function butao5(){
-    document.querySelector('.tecla_toim');
     document.querySelector('#som_tecla_toim').play();
 }
 
 function butao6(){
-    document.querySelector('.tecla_psh');
     document.querySelector('#som_tecla_psh').play();
 }
 
 function butao7(){
-    document.querySelector('.tecla_tic');
     document.querySelector('#som_tecla_tic').play();
 }
 
 function butao8(){
-    document.querySelector('.tecla_tom');
     document.querySelector('#som_tecla_tom').play();
 }
 
