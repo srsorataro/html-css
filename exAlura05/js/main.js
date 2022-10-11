f423864c5906d636c1bd0628ed7fb7f0ad60f3f1
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-11 15:30:12 -0300

    jgh

--- exAlura05/js/main.js
+++ exAlura05/js/main.js
@@ -1,24 +1,34 @@
-
- const  teclasList  =  document.querySelectorAll('.tecla');
- let contador = 0;
- while(contador <   teclasList.length ){
-    teclasList[contador].onclick = tocaSom;
-    contador  += 1 
-    console.log(contador)
-
-
+const teclasList = document.querySelectorAll('.tecla');
+function tocaS(son){
+    document.querySelector(son).play()
 }
 
- //const  audio =  document.querySelectorAll('audio');
-
-/*teclasList.forEach((elem,i ) => {
-    elem.onclick = () =>{
-        audio[i].play();
-        
+/*const adio  =  document.querySelectorAll('audio');
+teclasList.forEach((elem,i)=>{
+    elem.onclick = () => {
+        adio[i].play();
     }
-   
-})*/
-function tocaSom(som){
-     document.querySelector(som).play();
-   
+})
+*/
+
+for(let i = 0; i < teclasList.length; i++){
+    const tecla = teclasList[i]
+    const isntu = tecla.classList[1];
+    const idDio  = `#som_${isntu}`
+    tecla.onclick  = () =>{
+        tocaS(idDio);
+    }
+    tecla.onkeydown = function (){   
+        tecla.classList.add('ativa');
+       
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+
+    }
+    
 }
+
+
+
+
