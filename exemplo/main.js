e9924b6365741960a40abf8b2795bba666bb95c9
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-12 07:58:48 -0300

    Update main.js

--- exemplo/main.js
+++ exemplo/main.js
@@ -1,13 +1,23 @@
 const buto  = document.querySelectorAll('input[type=button]');
 const tel = document.querySelector('input[type=tel]');
 const clear = document.querySelector('#clear');
-function telefone(){
+
     for(let i = 0; i < buto.length; i++){
         buto[i].onclick = () =>{
             tel.value += buto[i].value
     
         }
+        buto.onkeydown  = function (e){
+            if(e.code === "Space"){
+                buto[i].classList.add('ativa')
+            }
+        }
+        buto.onkeydown  = function (e){
+            if(e.code === "Space"){
+                buto[i].classList.remove('ativa')
+            }
+        }
     }
-}
-telefone();
+
+
 
