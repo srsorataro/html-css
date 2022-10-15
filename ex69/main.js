937aa3b038fa51c9da45b1c72058525650540611
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-15 13:38:49 -0300

    64152

--- ex69/main.js
+++ ex69/main.js
@@ -2,3 +2,21 @@ let tela = document.querySelector('canvas');
 let pincel  = tela.getContext('2d');
 pincel.fillStyle ="lightgray";
 pincel.fillRect(0,0,600,400);
+
+pincel.fillStyle = "green";
+pincel.fillRect(0,0,100,400);
+
+pincel.fillStyle = "red";
+pincel.fillRect(200,0,200,400);
+
+pincel.fillStyle = "yellow";
+pincel.beginPath();
+pincel.moveTo(200,100);
+pincel.lineTo(153,20);
+pincel.lineTo(100,100);
+pincel.fill();
+
+pincel.fillStyle="blue";
+pincel.beginPath();
+pincel.arc(151,70,8,0,2 * 3.14);
+pincel.fill();
