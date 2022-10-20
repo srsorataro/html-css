1e82ca439259fddeecafb596ee3871f4b03c307b
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-20 08:07:45 -0300

    Update main.js

--- desafios/main.js
+++ desafios/main.js
@@ -1,2 +1,23 @@
-
 /*Math.floor(Math.random() * (máximo - mínimo + 1) + mínimo)*/
+/*const adivinha = Math.floor(Math.random() * (10 - 0 + 1 ) + 0);
+let chute = '';
+let acertou = false;
+for(let contador = 0; contador < 3; contador++){
+    chute  = prompt("tente adivinhar qual é o numero de  0 a 10");
+    if(chute == adivinha){
+        alert(`"você acertou o número era ${adivinha} "`)
+        acertou = true;
+        break
+    }
+    alert('errado')
+}
+if(!acertou){
+    alert(`"Infelismente você não acertou o suficiente! o numero era  ${adivinha}
+    "`)
+}*/
+
+function somar(somar1,somar2){
+    resultado = somar1 + somar2
+    console.log(resultado)
+}
+somar(100,110);
