22f4a1d010b6535321008d58e181b94678a48669
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-10-23 08:09:44 -0300

    45896

--- bandeira/bandeira.js
+++ bandeira/bandeira.js
@@ -1,9 +1,59 @@
 const tela  = document.querySelector('canvas');
 const pincel = tela.getContext('2d');
+let xAleatorio = 0;
+let yAleatorio = 0;
+function quadro(){
 
-
-pincel.fillStyle  ='red'
+pincel.fillStyle  ='lightgray'
 pincel.fillRect(0,0,1000,400);
 
+}
 
-pincel.fillStyle
+let raio = 10;
+function desenhaCirculo(x,y,raio,cor){
+    pincel.fillStyle = cor;
+    pincel.beginPath();
+    pincel.arc(x,y,raio,0,2*Math.PI);
+    pincel.fill();
+
+}
+function limaTela(){
+    pincel.clearRect(0,0,1000,400)
+}
+function desenhaAlvo(x,y){
+desenhaCirculo(x,y,raio+20,'red');
+desenhaCirculo(x,y,raio+10,'white');
+desenhaCirculo(x,y,raio,'red');
+
+}
+
+
+function sorteiaPosiçao(maximo){
+    return Math.floor(Math.random() * maximo)
+
+}
+function atualizaTela(){
+    limaTela();
+    quadro();
+    xAleatorio =  sorteiaPosiçao(1000);
+    yAleatorio =  sorteiaPosiçao(400);
+
+    desenhaAlvo(xAleatorio,yAleatorio);
+
+
+}
+function dispara(event){
+   let x = event.pageX - tela.offsetLeft;
+   let y = event.pageY - tela.offsetTop;
+  
+    if(x > xAleatorio - raio && 
+        x < xAleatorio + raio && 
+        y > yAleatorio - raio && 
+        y < yAleatorio + raio ){
+        alert("💥Acertou!💥")
+
+    }
+}
+tela.onclick = dispara;
+setInterval(atualizaTela,1000)
+
