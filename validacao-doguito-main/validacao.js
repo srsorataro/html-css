f65f086cd547bee75ec6bf097107001e4b53b4fe
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-11-23 17:08:13 -0300

    form.12

---
+++ validacao-doguito-main/validacao.js
@@ -0,0 +1,39 @@
+ export function valida(input){
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+
+    }
+}
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+const dataNascimento = document.querySelector('#nascimento')
+dataNascimento.addEventListener('blur', (evento) => {
+    validaDataNascimento(evento.target)
+    
+
+})
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date(input.value) 
+    let mensagem = ''
+    if(!maiorQue18(dataRecebida)){
+ 
+    mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+  
+}
+input.setCustomValidity(mensagem)
+
+}
+function maiorQue18(data){
+    const  dataAtual = new Date();
+    const dataMais18 = new Date(
+        data.getUTCFullYear() + 18,
+         data.getUTCMonth(), 
+         data.getUTCDate() )
+    return dataMais18 <= dataAtual
+
+}
