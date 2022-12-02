7b5de0005a23b7a5fbcbcf26a92b7f5a3abec4b2
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-12-02 17:31:20 -0300

    421

---
+++ -robotron-2000-projeto_inicial/main.js
@@ -0,0 +1,18 @@
+const botao = document.querySelector('#calcular')
+const resul = document.querySelector('.resultado')
+
+botao.addEventListener('click', () =>{
+  
+        botao.classList.toggle('ativo')
+        
+            resul.innerHTML = "Rapazzzz"
+
+        
+       
+    
+      
+
+   
+
+   
+})
