1a312272d71aa7ef70413b57b1eddbb7193e4667
srsorataro <107858903+srsorataro@users.noreply.github.com>
2022-12-30 04:57:39 -0300

    425

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,17 +1,7 @@
-const  form = document.getElementById('novo-item')
+const  form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
 const items = JSON.parse(localStorage.getItem("items")) || []
-items.forEach( e=> {
-  const novoItem =  document.createElement('li')
-  novoItem.classList.add('item')
-  const numeroItem = document.createElement('strong')
-  numeroItem.innerHTML = quantidade
- novoItem.appendChild(numeroItem)
- novoItem.innerHTML +=  nome 
- 
- lista.appendChild(novoItem)
 
-})
 
 form.addEventListener('submit',(event)=>{
    
@@ -19,9 +9,9 @@ form.addEventListener('submit',(event)=>{
     const nome = event.target.elements['nome']
     const quantidade = event.target.elements['quantidade']
     
-   criaElemento(nome.value,quantidade.value) 
-   nome.value =''
-  quantidade.value =''
+   criaElemento(nome.value, quantidade.value) 
+   nome.value = ''
+  quantidade.value = ''
     
 })
 function criaElemento(nome,quantidade){
@@ -38,5 +28,5 @@ const itemAtual = {
     "quantidade": quantidade
 }
 items.push(itemAtual)
-localStorage.setItem('items',JSON.stringify(itemAtual))
+localStorage.setItem('item',JSON.stringify(items))
 }
