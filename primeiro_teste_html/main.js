a05997200e8df91e3ec5de184f74ba085a9c3296
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-01-30 04:35:03 -0300

    Update main.js

--- primeiro_teste_html/main.js
+++ primeiro_teste_html/main.js
@@ -10,3 +10,16 @@ function mostrar(frase){
 
 }
 let  numeropP  = Math.round(Math.random() * 10)
+ let tentantivas = 1
+while(tentantivas <= 10){
+    let chute = parseInt(prompt('Digite seu chute:'))
+    if(chute == numeropP){
+        mostrar("Acertou!")
+        break
+    }else{
+        mostrar("Você errou, onúmero pensado foi" + numeropP)
+    }
+
+tentantivas++
+}
+mostrar('Fim')
