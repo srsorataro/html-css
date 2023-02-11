ff7c2322c9a380b3fabc9d44c5c27094a42dfe59
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-02-11 07:08:43 -0300

    Update pacman.js

--- pac01/pacman.js
+++ pac01/pacman.js
@@ -65,7 +65,33 @@ class Pacman{
     changeAnimation(){
         this.currentFrame = this.currentFrame == this.frameCount ? 1: this.currentFrame + 1     
     }
-    draw(){}
+    draw(){
+        canvasContext.save()
+        canvasContext.translate(
+            this.x  + oneBlockSize/2,
+            this.y + oneBlockSize / 2
+            ) 
+            canvasContext.rotate((this.direction * 90*Math.PI)/180)
+
+            canvasContext.translate(
+                -this.x  - oneBlockSize/2,
+                -this.y - oneBlockSize / 2
+                ) 
+                canvasContext.drawImage(
+                    pacmanFrames,(this.currentFrame - 1) /oneBlockSize,
+                    0,
+                    oneBlockSize,
+                    oneBlockSize,
+                    this.x,
+                    this.y,
+                    this.width,
+                    this.height
+                )
+                  
+                
+            
+            canvasContext.restore()
+          }
     getMapX(){
         return parseInt(this.x / oneBlockSize)
     }
