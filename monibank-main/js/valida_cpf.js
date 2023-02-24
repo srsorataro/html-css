4de4f81d540a37944956b618e47a36a3935cf393
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-02-24 04:36:16 -0300

    125

--- monibank-main/js/valida_cpf.js
+++ monibank-main/js/valida_cpf.js
@@ -1,8 +1,11 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepitidos(cpf)
-    console.log(validaNumerosRepitidos(cpf))
-    validaPrimeiroDigito(cpf)
+    if(validaNumerosRepitidos(cpf)||validaPrimeiroDigito(cpf)||validaSegundoDigito(cpf)){
+        console.log("esse cpf não existe")
+    }else{
+        console.log("esse cpf existe")
+    }
+
 }
 
 function validaNumerosRepitidos(cpf){
@@ -34,5 +37,20 @@ function validaPrimeiroDigito(cpf){
     }
     return soma != cpf[9]
 }
+
+function validaSegundoDigito(cpf){
+    let soma = 0; 
+    let multiplicador = 11;
+    for(let tamanho = 0; tamanho < 10; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    soma =(soma * 10)  % 11
+    if(soma == 10 || soma  == 11){
+        soma = 0
+    }
+    return soma != cpf[10]
+}
+ 
  
   
