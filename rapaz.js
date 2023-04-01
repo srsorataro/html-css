cbde36a999f252779bd8b59b0c826a9219b0c40a
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-01 07:33:56 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,12 +1,3 @@
-var respostaDeTudo = 42
-let idade = 29
-const pi = 3.14
-
-{
-    var respostaDeTudo = 3.14
-    let idade = 42
-    const pi = 29
-    console.log(respostaDeTudo, idade, pi)
-
-}
-console.log(respostaDeTudo, idade, pi)
+const nome = "ju"
+const apresentaçao = "meu noe é " + nome
+console.log(apresentaçao)
