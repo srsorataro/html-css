5a8a6161806d07ce6371076506540fcf4bdfd4fd
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-02 07:01:02 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,3 +1,7 @@
-const nome = "ju"
-const apresentaçao = "meu noe é " + nome
-console.log(apresentaçao)
+function soma(N1,N2){
+    return N1 + N2 
+}
+function multiplica(soma1,soma2){
+     return soma1 / soma2
+}
+console.log(multiplica(soma(15,45),soma(45,25)) )
