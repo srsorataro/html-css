9ca6066af32740d04e5071ee7c1a3aab5726ffa1
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-08 05:39:43 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,6 +1,15 @@
 const alunos  = ["João", "Juliana", "Ana", "Caio"]
 const medias =[10, 8, 7.5, 9]
 const  listaDeAlunosEMedias = [alunos,medias]
-for(let i = 0; i < alunos.length; i++){
-   console.log(alunos[i],medias[i])
+function exibirNomeDAluno(aluno){
+   if(listaDeAlunosEMedias[0].includes(aluno)){
+      const indice = listaDeAlunosEMedias[0].indexOf(aluno)
+      const mediaDoAluno = listaDeAlunosEMedias[1][indice]
+      console.log(`${aluno} tem a MEdia ${mediaDoAluno}`)
+
+   }else{
+      console.log("Aluno não encontrado")
+   }
+
 }
+exibirNomeDAluno("Ana")
