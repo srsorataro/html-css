1f66050fac0f08c5dd3407b814e3ca2bf7a20d1d
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-10 04:34:22 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,7 +1,5 @@
-const notas  = [7,75,82,93]
-let somaDasNotas = 0
-for(let elemet of  notas ){
-   somaDasNotas += elemet
-
+const  nun = [12,14,54,78,445]
+const n = nun / 5
+for(let i = 0;  i < n.length; i++){
+   console.log(n[i])
 }
-console.log(somaDasNotas)
