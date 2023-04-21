5d5def2d87874451d9aa2b75ba5c4e378a0d3deb
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-21 15:42:03 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,6 +1,19 @@
-const mulher = {
-    nome: "Luma",
-    profisssao:"engenheira",
+const cliente = {
+     nome:"João",
+     idade:24,
+     email:"João@firma.com",
+     telefone:[154521545456,87856465465465],
+     saldo: 200,
+     pagamento: function (valor) {
+        if(valor > this.saldo){
+            console.log("Saldo insuficiente")
+        }else{
+            this.saldo -= valor
+            console.log(`Saldo  suficiente ${this.saldo}`)
+        }
+
+     },
+ 
 }
-mulher.nome ="Luma silva"
-console.log(mulher.nome)
+
+cliente.pagamento(50)
