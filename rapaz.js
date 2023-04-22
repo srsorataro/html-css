d34c074099728b2e327fbd0c1b81bc3d174fa7f1
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-22 09:27:03 -0300

    d

--- rapaz.js
+++ rapaz.js
@@ -1,28 +1,8 @@
-const cliente = {
-     nome:"João",
-     idade:24,
-     email:"João@firma.com",
-     telefone:[154521545456,87856465465465],
-     saldo: 200,
+const dados = require("./cliente.json")
 
-}
-cliente.endereco =[
-    {
-    rua:"sergipe",
-    numero:41564,
-    apartamento:true,
-    complemento:"ap7821"
-},
-]
 
-function ligaParaCliente(telefoneCormecial,telefoneResidencial){
-    console.log(`"Ligando para o  telefone "${telefoneCormecial}`)
-    console.log(`"Ligando para o  telefone "${telefoneResidencial}`)
-}
-ligaParaCliente(...cliente.telefone)
-const ecomenda = {
-    destinatario: cliente.nome,
-    ...cliente.endereco[0]
-   
-}
-console.log(ecomenda)
+const  clienteEmString = JSON.stringify(dados)
+
+
+const  objetoCliente = JSON.parse(clienteEmString)
+console.log(objetoCliente)
