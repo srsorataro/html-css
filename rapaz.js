ea83ab72cc080bc061918f847c8c59cbb7acb2ab
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-04-24 05:15:32 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,23 +1,4 @@
-const Livro = function (nome,editora,paginas) {
-    gNome = nome,
-    gEditora = editora,
-    gPaginas = paginas
-
-    this.getNome = () =>{
-        return gNome
-
-    }
-    this.getEditora = () =>{
-        return gEditora
-
-    }
-    this.getpagina = () =>{
-        return gPaginas
-
-    }
-
-}
-const Grafico  = new Livro("Grafico","casa do Codigo",125)
-console.log(Grafico.getNome())
-console.log(Grafico.getEditora())
-console.log(Grafico.getpagina())
+const numertosPares = [2,4,6]
+const numerosImpares = [1,3,5]
+const numeros = [...numertosPares,...numerosImpares]
+console.log(numeros)
