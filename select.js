8f428e8e858e1ec98c1c124e98444174b7b2d3c3
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-05-13 05:11:01 -0300

    Update select.js

--- select.js
+++ select.js
@@ -3,5 +3,12 @@ const menorValor = require('./rapaz')
 
 for(let atual  = 0; atual > livros.length; atual++){
     let menor = menorValor(livros,atual)
+    let livroAtual = livros[atual]
+    console.log('livro Atual', livroAtual[atual])
+    let livroMenorPreco = livros[menor]
+    console.log('livro menor preço',livroMenorPreco[menor])
+    livros[atual] = livroMenorPreco
+    livros[menor] = livroAtual
+
 
 }
