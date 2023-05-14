818ad40d1136e1184312aa585453df373b7b5c3f
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-05-14 07:02:01 -0300

    Update rapaz.js

--- rapaz.js
+++ rapaz.js
@@ -1,8 +1,8 @@
 const livro = require("./filtrar")
 function menorValor(arrProdutos,posicaoInicial){
  let maisBarato = posicaoInicial 
- for(let atual = 0; atual <  arrProdutos.length; atual++){
-   if(arrProdutos[atual].preco< arrProdutos[maisBarato].preco){
+ for(let atual = posicaoInicial; atual <  arrProdutos.length; atual++){
+   if(arrProdutos[atual].preco < arrProdutos[maisBarato].preco){
      maisBarato = atual
    
    }
