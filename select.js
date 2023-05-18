c5cf22259008d4786a6f9f86f971f48264f62d2f
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-05-18 05:28:42 -0300

    Update select.js

---
+++ select.js
@@ -0,0 +1,8 @@
+const  livros  = require("./filtrar")
+const menorValor = require("./rapaz")
+
+for(let i =  0; i < livros.length; i++){
+    let menor = menorValor(livros,i)
+    let livroAtual = livros[i]
+    console.log(livroAtua)
+}
