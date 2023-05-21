51540417fb80cbf28ddc878fa9b2c2b2166026d3
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-05-21 15:54:05 -0300

    Update inserindoOrdem.js

--- inserindoOrdem.js
+++ inserindoOrdem.js
@@ -1,16 +1,21 @@
-const livros =  require("./filtrar")
-function isecaoordem(lista){
-    for(let i = 0; i < lista.length; i++){
-        let analise = i 
-        while( analise > 0 &&  lista[i].preco < lista[analise - 1].preco){
-            let itemanalise = lista[analise]
-            let itemAnterior = lista[analise - 1]
-            lista[i] = itemAnterior
-            lista[analise - 1] = itemanalise
-
+const livros =require('./filtrar')
+function inserir(lista){
+    for(let i  = 0; i < lista.length; i++){
+        let analise = i
+        while(analise > 0 && lista[analise].preco < lista[analise - 1].preco){
+            let itemAnterior = lista[analise - 1 ]
+            let itemAnalise = lista[analise]
+            lista[analise] = itemAnterior
+            lista[analise - 1] = itemAnalise
             analise--
+            
+
+          
+
         }
+       
     }
     console.log(lista)
+
 }
-isecaoordem(livros)
+inserir(livros)
