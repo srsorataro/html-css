3084300f41528727a6ebbaefd6667ee3d03a0d73
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-06-11 10:26:37 -0300

    Update main.js

--- hario/main.js
+++ hario/main.js
@@ -1,3 +1,6 @@
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+
 function loadImage(url){
     return new Promise(resolve =>{
         const image = new Image()
@@ -7,22 +10,47 @@ function loadImage(url){
         image.src = url
     })
 }
+class SpriteSheet{
+    constructor(image,width = 10,height = 10){
+        this.image  = image
+        this.width = width
+        this.height = height
+        this.tiles = new Map()
+    }
+    define(name,x,y){
+        const buffer = document.createElement('canvas')
+        buffer.width = this.width
+        buffer.height = this.height
+        buffer
+        .getContext('2d')
+        .drawImage(this.image,
+            x * this.width,
+            y * this.height,
+            this.width,
+            this.height,
+            0,
+            0,
+            this.width,
+            this.height) 
+            this.tiles.set(name,buffer)
+    }
+    draw(name,context,x,y){
+         
+        const buffer = this.tiles.get(name)
+       context.drawImage(buffer,x = 400,y = 200)
 
-const canvas = document.getElementById('screen')
-const context = canvas.getContext('2d')
+    }
+}
+
 
 context.fillRect(0,0,200,200)
 
  loadImage('./img/tiles.png')
  .then(image =>{
     const sprites = new SpriteSheet(image)
-    sprites.definte('ground',0,0)
-    sprites.draw('ground',context,45,45)
+    sprites.define('ground',0,0)
+    sprites.draw('ground',context,45,62)
 
 
-    context.drawImage(image,
-        0,0,
-        16,16,
-        32,32,
-        50,50)
+   
  })
