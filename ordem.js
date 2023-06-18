dd813af5db287cf532d491335205b01c2359665d
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-06-18 08:35:41 -0300

    Update ordem.js

--- ordem.js
+++ ordem.js
@@ -2,8 +2,9 @@ const livros =require('./filtrar')
 function mergeSort(array){
     if(array.length > 1 ){
         const meio = Math.floor(array.length / 2)
-        const p1 = array.slice(0,meio)
-        const p2 = array.slice(meio,array.length)
+        const p1 = mergeSort(array.slice(0,meio)) 
+        const p2 =  mergeSort(array.slice(meio,array.length)) 
+        array  =ordena(p1,p2)
 
     }
     return array
