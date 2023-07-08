f720c9fd6d510f4f472c08e0f2d50bfebbb14179
srsorataro <107858903+srsorataro@users.noreply.github.com>
2023-07-08 08:29:40 -0300

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,14 @@
+const  livros = require('./filtrar')
+
+function quickSort(array, esqueda, direita){
+    if(array.length > 1) {
+        let varTemp =  particionar(array,esquerda,direita)
+    }
+    function particionar(array, esquerda, direita){
+    let pivo = array [Math.floor((esquerda + direita) / 2)]
+    
+}
+
+    return array
+}
+console.log( quickSort   (livros,0, livros.length - 1))
